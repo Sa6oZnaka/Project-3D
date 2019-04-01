fd16b7ce882b1cbce7cd4e5ead809431e7f70b67
Alexandar Karapenev <treildr@gmail.com>
2019-04-01 10:28:17 +0300

    Object is now moving and have texture

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
+
+var x = -275,
+    y =  175;
+
+
+let img;
+function preload(){
+    img = loadImage('assets/cube.png');
+}
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+}
+
+function Input(){
+    if (keyIsDown(LEFT_ARROW)) {
+        x -= 5;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        x += 5;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        y -= 5;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        y += 5;
+    }
 }
 
 function draw() {
-  background(100);
 
-  noStroke();
-  fill(50);
-  push();
-  translate(-275, 175);
-  rotateY(1.25);
-  rotateX(-0.9);
-  box(100);
-  pop();
+    background(100);
 
-  noFill();
-  stroke(255);
-  push();
-  translate(500, height * 0.35, -200);
-  sphere(300);
-  pop();
+    noStroke();
+    fill(50);
+    push();
+    translate(x, y);
+    //rotateY(1.25);
+    //rotateX(-0.9);
+    texture(img);
+    box(200, 200, 200);
+    pop();
+
+
+    noFill();
+    stroke(255);
+    push();
+    translate(500, height * 0.35, -200);
+    sphere(300);
+    pop();
+
+
+    Input();
 }
