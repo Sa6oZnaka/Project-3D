c5459b612e94c26b3792d0f72fdf387820f5f2b8
Alexandar Karapenev <treildr@gmail.com>
2019-04-02 10:42:45 +0300

    Added car model camera now is working

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 
 var x = -275,
     y =  175,
-    z =   10;
+    z =   10,
+
+    angle = 0, 
+    speed = 0;
+
+
+var sizeX = 100;
+var sizeY = 60;
+var sizeZ = 300;
 
 let img;
 function preload(){
     img = loadImage('assets/cube.png');
+
+    car = loadModel('assets/Porsche_911_GT2/Porsche_911_GT2.obj');
+    //car = loadModel('assets/huracan in 2.8.obj');
+
+    //car = loadModel('assets/BMW X5 4.obj')
+
 }
 
 function setup() {
@@ -15,36 +29,68 @@ function setup() {
 
 function Input(){
     if (keyIsDown(LEFT_ARROW)) {
-        x -= 5;
+        angle -= PI / 18;
     }
     if (keyIsDown(RIGHT_ARROW)) {
-        x += 5;
+        angle += PI / 18;
     }
     if (keyIsDown(UP_ARROW)) {
-        y -= 5;
+        speed -= 0.5;
     }
     if (keyIsDown(DOWN_ARROW)) {
-        y += 5;
+        speed += 0.5;
     }
 }
 
 function draw() {
 
-    let camY = map(mouseY, 0, width, -200, 0);
-    let camX = map(mouseX, 0, width, -200, 0);
+    if(angle != 0){
+        if(angle > 0){
+            x -= speed * angle;
+            angle -= (speed * angle)/10;
+        }else{
+            x += speed * angle;
+            angle += (speed * angle)/10;
+        }
+    }
+
+    z += speed;
+    speed = speed / 1.02;
+
+    let camY = 30; 
+    let camX = x;//map(mouseX, 0, width, -200, 0);
+    let camZ = z;
+
     //camera(camX, camY, (height/2) / tan(PI/6), x, y, z, 0, 1, 0);
-    camera(camX, camY, (height/2) / tan(PI/6), x, y, z, 0, 1, 0);
+    camera(camX, camY, 400 + z, x, y, z, 0, 1, 0);
+
+
 
     background(100);
 
-    noStroke();
-    fill(50);
+    //noStroke();
+    //fill(50);
     push();
     translate(x, y, z);
-    //rotateY(1.25);
-    //rotateX(-0.9);
-    texture(img);
-    box(200, 200, 200);
+    rotateZ(PI);
+    rotateY(angle);
+    //texture(img);
+
+    scale(50);
+    //model(car);
+    //box(sizeX, sizeY, sizeZ);
+        //pop();
+
+    //normalMaterial(255);
+
+    //background(200);
+      //rotateX(frameCount * 0.01);
+      //rotateY(frameCount * 0.01);
+    model(car);
+    //normalMaterial(255);
+
+
+
     pop();
 
 
